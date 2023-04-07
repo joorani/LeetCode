d625024c5b80089382fdeff6404ef66375e626c7
joorani <80900915+joorani@users.noreply.github.com>
2023-04-07 23:13:42 +0900

    Time: 8 ms, Memory: 14.2 MB - LeetHub

--- remove-element/remove-element.swift
+++ remove-element/remove-element.swift
@@ -1,14 +1,14 @@
 
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        var begin: Int = 0
+        var index: Int = 0
         for i in 0..<nums.count {
             if nums[i] != val {
-                nums[begin] = nums[i]
-                begin += 1
+                nums[index] = nums[i]
+                index += 1
             }
         }
-        print(nums)
-        return begin
+
+        return index
     }
 }
