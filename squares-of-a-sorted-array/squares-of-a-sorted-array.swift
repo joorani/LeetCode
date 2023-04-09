bc193a0fb1e901a2c1dc09b4bf005119ff224065
joorani <80900915+joorani@users.noreply.github.com>
2023-04-09 19:16:23 +0900

    Time: 204 ms, Memory: 15.8 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.swift
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.swift
@@ -1,5 +1,27 @@
 class Solution {
     func sortedSquares(_ nums: [Int]) -> [Int] {
-        return nums.map { $0 * $0 }.sorted()
+        // return nums.map { $0 * $0 }.sorted()
+        
+        // two pointers
+        var index = nums.count - 1
+        var left = 0
+        var right = nums.count - 1
+        
+        var resultNums = nums
+        
+        while index >= 0 {
+            if abs(nums[left]) < abs(nums[right]) {
+                resultNums[index] = nums[right] * nums[right]
+                right -= 1
+            } else {
+                resultNums[index] = nums[left] * nums[left]
+                left += 1
+            }
+            
+            index -= 1
+        }
+        
+        
+        return resultNums
     }
 }
